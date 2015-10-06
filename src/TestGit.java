1a7700e74995f0ab4ebf9152adb0d01bf2b61ea6
Jim <Jim@JimsPC>
2015-10-06 13:12:00 -0400

    This is a test

--- src/TestGit.java
+++ src/TestGit.java
@@ -3,7 +3,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a modified test\n");
+		System.out.println("This is a really modified test\n");
 
 	}
 
